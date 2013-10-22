101a5ec187ea74b42b8aec61a18d59d2687efd54
Andrew Hilobok <hilobok@gmail.com>
2013-10-22 11:55:35 +0300

    Auto prepend assetic bundles

--- DependencyInjection/AnhPagerExtension.php
+++ DependencyInjection/AnhPagerExtension.php
@@ -6,13 +6,14 @@ use Symfony\Component\DependencyInjection\ContainerBuilder;
 use Symfony\Component\Config\FileLocator;
 use Symfony\Component\HttpKernel\DependencyInjection\Extension;
 use Symfony\Component\DependencyInjection\Loader;
+use Symfony\Component\DependencyInjection\Extension\PrependExtensionInterface;
 
 /**
  * This is the class that loads and manages your bundle configuration
  *
  * To learn more see {@link http://symfony.com/doc/current/cookbook/bundles/extension.html}
  */
-class AnhPagerExtension extends Extension
+class AnhPagerExtension extends Extension implements PrependExtensionInterface
 {
     /**
      * {@inheritDoc}
@@ -24,5 +25,14 @@ class AnhPagerExtension extends Extension
 
         $loader = new Loader\YamlFileLoader($container, new FileLocator(__DIR__.'/../Resources/config'));
         $loader->load('services.yml');
+    }
+
+    public function prepend(ContainerBuilder $container)
+    {
+        $container->prependExtensionConfig('assetic', array(
+            'bundles' => array(
+                'AnhPagerBundle'
+            )
+        ));
     }
 }
